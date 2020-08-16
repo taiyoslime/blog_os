4e5b6e46cc656b13c54944164c2d05a8814f7fa9
taiyoslime <t@iyosli.me>
2020-08-16 22:26:47 +0900

    Complete chap-3

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
 extern crate rlibc;
+use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
+
+static HELLO: &str = "Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	let vga_buffer = 0xb8000 as *mut u8;
-
-	for (i, &byte) in HELLO.iter().enumerate() {
-		unsafe {
-			*vga_buffer.offset(i as isize * 2) = byte;
-			*vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-		}
-	}
+	println!("{}", HELLO);
 	loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+	println!("{}", info);
 	loop {}
 }
