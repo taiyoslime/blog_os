f671628326159e7583661a6f4944d873196f78a6
taiyoslime <t@iyosli.me>
2020-08-29 19:09:39 +0900

    complete chap-4

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,27 +7,19 @@
 use blog_os::println;
 use core::panic::PanicInfo;
 
-static HELLO: &str = "Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	println!("{}", HELLO);
-
-	#[cfg(test)]
 	test_main();
 
 	loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-	println!("{}", info);
-	loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
 	blog_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+	println!("test_println output");
 }
